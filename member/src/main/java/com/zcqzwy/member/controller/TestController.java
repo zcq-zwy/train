9abb266ba3d016c49df4d33df1fa446a4b956b82
zcq-zwy <2928235428@qq.com>
2026-02-23 12:08:48 +0800

    项目模块初始化

---
+++ member/src/main/java/com/zcqzwy/member/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.zcqzwy.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>作者： zcq</p> 
+ * <p>文件名称: TestController </p> 
+ * <p>描述: [类型描述] </p>
+ * <p>创建时间: 2026/2/23 </p>
+ * @author <a href="mail to: 2928235428@qq.com" rel="nofollow">作者</a>
+ * @version 1.0
+ **/
+@RestController
+public class TestController {
+  @GetMapping("/hello")
+  public String hello(){
+    return  "hello world";
+  }
+}
