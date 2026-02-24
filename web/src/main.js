40f71626c4a3f54dadfab49706df483d23798d8b
zcq-zwy <2928235428@qq.com>
2026-02-24 14:02:34 +0800

    集成ant-vue和其图标

--- web/src/main.js
+++ web/src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// 引入ant-vue
+import Antd from 'ant-design-vue';
+
+import 'ant-design-vue/dist/antd.css';
+
+import * as Icons from '@ant-design/icons-vue';
+
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app');
+
+// 全局使用图标
+const icons = Icons;
+for (const i in icons) {
+  app.component(i, icons[i]);
+}
+
--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button>Button</a-button><plus-outlined />
   </div>
 </template>
