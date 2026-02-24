2b6418aa08bbcc999b5866c9a9920cb875733fd5
zcq-zwy <2928235428@qq.com>
2026-02-24 19:52:32 +0800

    增加axios拦截器，打印请求参数和返回结果

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
 
 // 引入ant-vue
 import Antd from 'ant-design-vue';
@@ -18,4 +19,25 @@ const icons = Icons;
 for (const i in icons) {
   app.component(i, icons[i]);
 }
+
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+  console.log('请求参数：', config);
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+  console.log('返回结果：', response);
+  return response;
+}, error => {
+  console.log('返回错误：', error);
+  return Promise.reject(error);
+});
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);
 
